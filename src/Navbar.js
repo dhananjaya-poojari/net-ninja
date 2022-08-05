3d3e8cf383bdac9927f99c7c571080ce0205a103
Dhananjaya Poojari <DPoojari@magsw.com>
2022-08-05 18:43:20 +0530

    net-ninja

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>The Net Ninja</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">bar</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
